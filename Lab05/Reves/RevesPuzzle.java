18cab0f99abc5fa10268e40a48578130709c8a91
rchen30 <102829065+rchen30@users.noreply.github.com>
2022-04-04 08:40:02 -0400

    Update RevesPuzzle.java

--- Lab05/Reves/RevesPuzzle.java
+++ Lab05/Reves/RevesPuzzle.java
@@ -13,13 +13,14 @@ public class RevesPuzzle{
 	return n + 1 - (int) Math.pow(2 * n + 1, 0.5);
     }
     
-    public static void helper(int n){}
+    public static void helper(int n, int k, int start, int end){}
 
-    public static int spare(){return 1;}
+    public static int spare(){}
     
     public static void main(String args[]){
 	int n = Integer.parseInt(args[0]);
 	int k = findK(n);
+	//A = 1, B = 2, C = 3, D = 4
 	helper();
     }
 }
