2e6b9326233b531d462773871309341736d0a719
Jamie30 <53837369+JawadSifat@users.noreply.github.com>
2022-04-05 08:25:40 -0400

    Base case?

---
+++ Lab05/CodeStepByStep/WaysToClimb.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class WaysToClimb {
+
+	public static ArrayList<Integer> waysToClimbHelper(int n, ArrayList<Integer> out){
+		if (n == 1) { out.add(1); return out;}
+		out.add(1);
+		return waysToClimbHelper(n-1, out);
+	}
+
+	public static void waysToClimb(int n){
+		ArrayList<Integer> out = new ArrayList<Integer>();
+		StdOut.println(waysToClimbHelper(n, out));
+	}
+
+	public static void main(String[] args){
+		int n = StdIn.readInt();
+
+		StdOut.printf("Given the number of stairs is %d our output is ", n); 
+		waysToClimb(n);
+		StdOut.println();
+	}
+}
