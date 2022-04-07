1e38e68bd8777a3bf580a97529efa44b09e87240
rchen30 <102829065+rchen30@users.noreply.github.com>
2022-04-07 08:30:39 -0400

    Add files via upload

---
+++ Lab06/sierpinski/Sierpinski.java
@@ -0,0 +1,20 @@
+public class Sierpinski {
+
+    // Height of an equilateral triangle with the specified side length.
+    public static double height(double length){}
+
+    // Draws a filled equilateral triangle with the specified side length
+    // whose bottom vertex is (x, y).
+    public static void filledTriangle(double x, double y, double length){}
+
+    // Draws a Sierpinski triangle of order n, such that the largest filled
+    // triangle has the specified side length and bottom vertex (x, y).
+    public static void sierpinski(int n, double x, double y, double length){}
+
+    // Takes an integer command-line argument n;
+    // draws the outline of an upwards equilateral triangle of length 1
+    // whose bottom-left vertex is (0, 0) and bottom-right vertex is (1, 0);
+    // and draws a Sierpinski triangle of order n that fits inside the outline.
+    public static void main(String[] args){}
+
+}
