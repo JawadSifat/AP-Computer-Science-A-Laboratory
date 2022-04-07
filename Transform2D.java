bdbc2064a9549082651d525b9d5a4e3045206dfb
rchen30 <102829065+rchen30@users.noreply.github.com>
2022-04-07 08:01:02 -0400

    Add files via upload

---
+++ Transform2D.java
@@ -0,0 +1,43 @@
+public class Transform2D {
+
+    // Returns a new array object that is an exact copy of the given array.
+    // The given array is not mutated.
+    public static double[] copy(double[] array){
+	return array
+	    }
+
+    // Scales the polygon by the factor alpha.
+    public static void scale(double[] x, double[] y, double alpha){}
+
+    // Translates the polygon by (dx, dy).
+    public static void translate(double[] x, double[] y, double dx, double dy){}
+
+    // Rotates the polygon theta degrees counterclockwise, about the origin.
+    public static void rotate(double[] x, double[] y, double theta){}
+
+    // Tests each of the API methods by directly calling them.
+    public static void main(String[] args) {
+	// Set the x- and y-scale
+	StdDraw.setScale(-5.0, 5.0);
+
+	// Create original polygon
+	double[] x = { 0, 1, 1, 0 };
+	double[] y = { 0, 0, 2, 1 };
+
+	// Copy original polygon
+	double[] cx = copy(x);
+	double[] cy = copy(y);
+
+	// Rotate and translate the copy
+	rotate(cx, cy, -45.0);
+	translate(cx, cy, 1.0, 2.0);
+
+	// Draw the copy in blue
+	StdDraw.setPenColor(StdDraw.BLUE);
+	StdDraw.polygon(cx, cy);
+
+	// Draw the original polygon in red
+	StdDraw.setPenColor(StdDraw.RED);
+	StdDraw.polygon(x, y);
+    }
+}
