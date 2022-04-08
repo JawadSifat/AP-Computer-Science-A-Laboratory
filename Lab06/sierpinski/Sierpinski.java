d1baf9a9c5b0f008790a9d3bbbc5b7e9b1ebb255
rchen30 <102829065+rchen30@users.noreply.github.com>
2022-04-08 09:32:14 -0400

    Update Sierpinski.java
    sierpinski method is wip

--- Lab06/sierpinski/Sierpinski.java
+++ Lab06/sierpinski/Sierpinski.java
@@ -8,6 +8,9 @@ public class Sierpinski {
     // Draws a filled equilateral triangle with the specified side length
     // whose bottom vertex is (x, y).
     public static void filledTriangle(double x, double y, double length){
+	double[] a = {x, x - (height(length) / 2.0), x + (height(length) / 2.0)};
+	double[] b = {y, y + height(length), y + height(length)};
+	StdDraw.filledPolygon(a, b);
     }
 
     // Draws a Sierpinski triangle of order n, such that the largest filled
@@ -19,8 +22,10 @@ public class Sierpinski {
     // whose bottom-left vertex is (0, 0) and bottom-right vertex is (1, 0);
     // and draws a Sierpinski triangle of order n that fits inside the outline.
     public static void main(String[] args){
-	    double[] ufx = {0, 0.5, 1};
-	    double[] ufy = {0, (Math.sqrt(3) / 2.0),0};
-	    StdDraw.polygon(ufx, ufy);
+	int n = Integer.parseInt(args[0]);
+	double[] ufx = {0, 0.5, 1};
+	double[] ufy = {0, (Math.sqrt(3) / 2.0),0};
+	StdDraw.polygon(ufx, ufy);
+	sierpinski(n);
     }
 }
